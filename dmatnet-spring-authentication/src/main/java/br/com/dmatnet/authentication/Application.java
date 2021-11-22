89fe7019dcac06e3d31b1dbb21d27ad914487571
thiagobueno <thiagobueno@localhost>
2021-11-22 14:55:44 -0300

    ajustes nos nomes dos packages

--- dmatnet-spring-authentication/src/main/java/br/com/dmatnet/authentication/Application.java
+++ dmatnet-spring-authentication/src/main/java/br/com/dmatnet/authentication/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DmatnetSpringAuthenticationApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DmatnetSpringAuthenticationApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
