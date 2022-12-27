320679196412a19288bf26a68489cfe3e11014b0
thiagobueno <bueno_thiago@outlook.com>
2022-12-27 14:25:55 -0300

    Ajuste do encoding no POM e Início dos Testes Unitários

--- src/test/java/br/com/dmatnet/authentication/controller/UsuarioControllerTest.java
+++ src/test/java/br/com/dmatnet/authentication/controller/UsuarioControllerTest.java
@@ -1,25 +1,6 @@
 package br.com.dmatnet.authentication.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
-
-@SpringBootTest
-@AutoConfigureMockMvc(addFilters = false)
 public class UsuarioControllerTest {
 
-    @Autowired
-    MockMvc mockMvc;
-
-/*    @Test
-    public void shouldBe_SaveNewUser_Success() {
-
-        mockMvc.perform(MockMvcRequestBuilders
-                .post("/autenticacao")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content()
-
-    }*/
 
 }
